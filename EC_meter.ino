cb82bc8becb3e04bb3f0080b89bde9eb6fc25240
trooper5010 <phillengel@gmail.com>
2016-05-23 12:30:09 -0700

    Add files via upload

---
+++ EC_meter.ino
@@ -0,0 +1,103 @@
+// # 
+// # Editor     : YouYou from DFRobot
+// # Date       : 23.04.2014
+// # E-Mail	: youyou.yu@dfrobot.com
+
+// # Product name: Analog EC Meter
+// # Product SKU : DFR0300
+// # Version     : 1.0
+
+// # Description:
+// # Sample code for testing the EC meter and get the data feedback from the Arduino Serial Monitor.
+
+// # Connection:
+// #        EC meter output     -> Analog pin 1
+// #        DS18B20 digital pin -> Digital pin 2
+// #
+
+#include <OneWire.h>
+
+#define StartConvert 0
+#define ReadTemperature 1
+
+const byte numReadings = 20;     //the number of sample times
+byte ECsensorPin = A1;  //EC Meter analog output,pin on analog 1
+byte DS18B20_Pin = 2; //DS18B20 signal, pin on digital 2
+unsigned int AnalogSampleInterval=25,printInterval=700,tempSampleInterval=850;  //analog sample interval;serial print interval;temperature sample interval
+unsigned int readings[numReadings];      // the readings from the analog input
+byte index = 0;                  // the index of the current reading
+unsigned long AnalogValueTotal = 0;                  // the running total
+unsigned int AnalogAverage = 0,averageVoltage=0;                // the average
+unsigned long AnalogSampleTime,printTime,tempSampleTime;
+float temperature,ECcurrent; 
+
+
+void setup() {
+ // initialize serial communication with computer:
+  Serial.begin(9600);
+  // initialize all the readings to 0:
+  for (byte thisReading = 0; thisReading < numReadings; thisReading++)
+    readings[thisReading] = 0;
+  AnalogSampleTime=millis();
+  printTime=millis();
+  tempSampleTime=millis();
+}
+
+void loop() {
+  /*
+   Every once in a while,sample the analog value and calculate the average.
+  */
+  if(millis()-AnalogSampleTime>=AnalogSampleInterval)  
+  {
+    AnalogSampleTime=millis();
+     // subtract the last reading:
+    AnalogValueTotal = AnalogValueTotal - readings[index];
+    // read from the sensor:
+    readings[index] = analogRead(ECsensorPin);
+    // add the reading to the total:
+    AnalogValueTotal = AnalogValueTotal + readings[index];
+    // advance to the next position in the array:
+    index = index + 1;
+    // if we're at the end of the array...
+    if (index >= numReadings)
+    // ...wrap around to the beginning:
+    index = 0;
+    // calculate the average:
+    AnalogAverage = AnalogValueTotal / numReadings;
+  }
+   /*
+   Every once in a while,print the information on the serial monitor.
+  */
+  if(millis()-printTime>=printInterval)
+  {
+    printTime=millis();
+    averageVoltage=AnalogAverage*(float)5000/1024;
+    //Do not need the following readings for Soil Analysis system
+    //Serial.print("Analog value:");
+    //Serial.print(AnalogAverage);   //analog average,from 0 to 1023
+    //Serial.print("    Voltage:");
+    //Serial.print(averageVoltage);  //millivolt average,from 0mv to 4995mV
+    //Serial.print("mV    ");
+    //Serial.print("temp:");
+    //Serial.print(temperature);    //current temperature
+    Serial.print("EC:");
+    
+    float TempCoefficient=1.0+0.0185*(temperature-25.0);    //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.0185*(fTP-25.0));
+    float CoefficientVolatge=(float)averageVoltage/TempCoefficient;   
+    if(CoefficientVolatge<150)Serial.println("No solution!");   //25^C 1413us/cm<-->about 216mv  if the voltage(compensate)<150,that is <1ms/cm,out of the range
+    else if(CoefficientVolatge>3300)Serial.println("Out of the range!");  //>20ms/cm,out of the range
+    else
+    { 
+      if(CoefficientVolatge<=448)ECcurrent=6.84*CoefficientVolatge-64.32;   //1ms/cm<EC<=3ms/cm
+      else if(CoefficientVolatge<=1457)ECcurrent=6.98*CoefficientVolatge-127;  //3ms/cm<EC<=10ms/cm
+      else ECcurrent=5.3*CoefficientVolatge+2278;                           //10ms/cm<EC<20ms/cm
+      ECcurrent/=1000;    //convert us/cm to ms/cm
+      Serial.print(ECcurrent,2);  //two decimal
+      Serial.println("ms/cm");
+    }
+  }
+
+}
+/*
+ch=0,let the DS18B20 start the convert;ch=1,MCU read the current temperature from the DS18B20.
+*/
