ce3ab777440c893aa44b7a318f8b364b3c22ffa9
trooper5010 <phillengel@gmail.com>
2016-05-23 12:25:19 -0700

    Add files via upload
    Combined code of all four sensors in one void setup and one void loop.
    
    The four sensors in the code by description are:
    //EC meter
    //pH sensor
    //DHT22 temperature sensor
    //Moisture Sensor
    
    Each of individual sample codes for the sensors that were used are provided here:
    EC meter: http://www.dfrobot.com/wiki/index.php/Analog_EC_Meter_SKU:DFR0300#Sample_Code
    pH sensor: http://dfrobot.com/wiki/index.php/PH_meter(SKU:_SEN0161)#Sample_Code
    
    and the DHT sensor code is provided in the Github repository if needed.

---
+++ Combined1.ino
@@ -0,0 +1,273 @@
+// # EC meter
+// # Editor     : YouYou from DFRobot
+// # Date       : 23.04.2014
+// # E-Mail	: youyou.yu@dfrobot.com
+
+// # Product name: Analog EC Meter
+// # Product SKU : DFR0300
+// # Version     : 1.0
+
+// # Description:
+// # Sample code for testing the EC meter and get the data feedback from the Arduino Serial Monitor.
+
+// # Connection:
+// #        EC meter output     -> Analog pin 1
+// #        DS18B20 digital pin -> Digital pin 2
+// #
+
+#include <OneWire.h>
+#define StartConvert 0
+#define ReadTemperature 1
+const byte numReadings = 20;     //the number of sample times
+byte ECsensorPin = A1;  //EC Meter analog output,pin on analog 1
+byte DS18B20_Pin = 2; //DS18B20 signal, pin on digital 2
+unsigned int AnalogSampleInterval=25,printInterval=700,tempSampleInterval=850;  //analog sample interval;serial print interval;temperature sample interval
+unsigned int readings[numReadings];      // the readings from the analog input
+byte index = 0;                  // the index of the current reading
+unsigned long AnalogValueTotal = 0;                  // the running total
+unsigned int AnalogAverage = 0,averageVoltage=0;                // the average
+unsigned long AnalogSampleTime,printTime,tempSampleTime;
+float temperature,ECcurrent; 
+
+/* //pH sensor
+ # This sample code is used to test the pH meter V1.0.
+ # Editor : YouYou
+ # Ver    : 1.0
+ # Product: analog pH meter
+ # SKU    : SEN0161
+*/
+#define SensorPin A0            //pH meter Analog output to Arduino Analog Input 0
+#define Offset 0.00            //deviation compensate
+#define LED 13
+#define samplingInterval 20
+#define printInterval 800
+#define ArrayLenth  40    //times of collection
+int pHArray[ArrayLenth];   //Store the average value of the sensor feedback
+int pHArrayIndex=0;
+
+//DHT22 temperature sensor
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+#include "DHT.h"
+#define DHTPIN 2     // what digital pin we're connected to
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+
+//Moisture Sensor
+/*
+  # Example code for the moisture sensor
+  # Editor     : Lauren
+  # Date       : 13.01.2012
+  # Version    : 1.0
+  # Connect the sensor to the A0(Analog 0) pin on the Arduino board
+   
+  # the sensor value description
+  # 0  ~300     dry soil
+  # 300~700     humid soil
+  # 700~950     in water
+*/
+//
+
+void setup() {
+ // EC meter
+ // initialize serial communication with computer:
+  //Serial.begin(9600);
+  Serial.begin(115200);
+  // initialize all the readings to 0:
+  for (byte thisReading = 0; thisReading < numReadings; thisReading++)
+    readings[thisReading] = 0;
+  AnalogSampleTime=millis();
+  printTime=millis();
+  tempSampleTime=millis();
+
+  //pH meter
+  pinMode(LED,OUTPUT);  
+  Serial.begin(9600);
+  //Serial.begin(115200);  
+  Serial.println("pH meter experiment");    //Test the serial monitor
+
+  //DHT22 temperature sensor
+  Serial.begin(9600);
+  //Serial.begin(115200);
+  Serial.println("DHTxx test");
+  dht.begin();
+  
+  //Moisture sensor
+  //Serial.begin(57600);
+  Serial.begin(9600);
+  //Serial.begin(115200);
+}
+
+void loop() {
+  delay(1000);
+  
+  //EC meter
+  /*
+   Every once in a while,sample the analog value and calculate the average.
+  */
+  if(millis()-AnalogSampleTime>=AnalogSampleInterval)  
+  {
+    AnalogSampleTime=millis();
+     // subtract the last reading:
+    AnalogValueTotal = AnalogValueTotal - readings[index];
+    // read from the sensor:
+    readings[index] = analogRead(ECsensorPin);
+    // add the reading to the total:
+    AnalogValueTotal = AnalogValueTotal + readings[index];
+    // advance to the next position in the array:
+    index = index + 1;
+    // if we're at the end of the array...
+    if (index >= numReadings)
+    // ...wrap around to the beginning:
+    index = 0;
+    // calculate the average:
+    AnalogAverage = AnalogValueTotal / numReadings;
+  }
+   /*
+   Every once in a while,print the information on the serial monitor.
+  */
+  if(millis()-printTime>=printInterval)
+  {
+    printTime=millis();
+    averageVoltage=AnalogAverage*(float)5000/1024;
+    //Serial.print("Analog value:");
+    //Serial.print(AnalogAverage);   //analog average,from 0 to 1023
+    //Serial.print("    Voltage:");
+    //Serial.print(averageVoltage);  //millivolt average,from 0mv to 4995mV
+    //Serial.print("mV    ");
+    //Serial.print("temp:");
+    //Serial.print(temperature);    //current temperature
+    Serial.print("EC:");
+    
+    float TempCoefficient=1.0+0.0185*(temperature-25.0);    //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.0185*(fTP-25.0));
+    float CoefficientVolatge=(float)averageVoltage/TempCoefficient;   
+    if(CoefficientVolatge<150)Serial.println("No solution!");   //25^C 1413us/cm<-->about 216mv  if the voltage(compensate)<150,that is <1ms/cm,out of the range
+    else if(CoefficientVolatge>3300)Serial.println("Out of the range!");  //>20ms/cm,out of the range
+    else
+    { 
+      if(CoefficientVolatge<=448)ECcurrent=6.84*CoefficientVolatge-64.32;   //1ms/cm<EC<=3ms/cm
+      else if(CoefficientVolatge<=1457)ECcurrent=6.98*CoefficientVolatge-127;  //3ms/cm<EC<=10ms/cm
+      else ECcurrent=5.3*CoefficientVolatge+2278;                           //10ms/cm<EC<20ms/cm
+      ECcurrent/=1000;    //convert us/cm to ms/cm
+      Serial.print(ECcurrent,2);  //two decimal
+      Serial.println("ms/cm");
+    }
+  }
+  
+  //pH Sensor
+  static unsigned long samplingTime = millis();
+  static unsigned long printTime = millis();
+  static float pHValue,voltage;
+  if(millis()-samplingTime > samplingInterval)
+  {
+      pHArray[pHArrayIndex++]=analogRead(SensorPin);
+      if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
+      voltage = avergearray(pHArray, ArrayLenth)*5.0/1024;
+      pHValue = 3.5*voltage+Offset;
+      samplingTime=millis();
+  }
+  if(millis() - printTime > printInterval)   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
+  {
+    //Serial.print("Voltage:");
+    //    Serial.print(voltage,2);
+        Serial.print("pH value: ");
+    Serial.println(pHValue,2);
+        digitalWrite(LED,digitalRead(LED)^1);
+        printTime=millis();
+  }
+  
+  //DHT22 temperature sensor
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  //Serial.print("Humidity: ");  //The following is taken out because it is not needed
+  //Serial.print(h);
+  //Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  //Serial.print("Heat index: ");  //The following is taken out because it is not needed
+  //Serial.print(hic);
+  //Serial.print(" *C ");
+  //Serial.print(hif);
+  //Serial.println(" *F");
+  
+  //Moisture Sensor
+  Serial.print("Moisture Sensor Value:");
+  Serial.println(analogRead(A0));
+}
+
+//pH sensor functions
+double avergearray(int* arr, int number){
+  int i;
+  int max,min;
+  double avg;
+  long amount=0;
+  if(number<=0){
+    Serial.println("Error number for the array to avraging!/n");
+    return 0;
+  }
+  if(number<5){   //less than 5, calculated directly statistics
+    for(i=0;i<number;i++){
+      amount+=arr[i];
+    }
+    avg = amount/number;
+    return avg;
+  }else{
+    if(arr[0]<arr[1]){
+      min = arr[0];max=arr[1];
+    }
+    else{
+      min=arr[1];max=arr[0];
+    }
+    for(i=2;i<number;i++){
+      if(arr[i]<min){
+        amount+=min;        //arr<min
+        min=arr[i];
+      }else {
+        if(arr[i]>max){
+          amount+=max;    //arr>max
+          max=arr[i];
+        }else{
+          amount+=arr[i]; //min<=arr<=max
+        }
+      }//if
+    }//for
+    avg = (double)amount/(number-2);
+  }//if
+  return avg;
+}
+
